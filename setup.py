93dc786b43382239ff9f00b2f5732fb9df5125cc
Matthias Baer <matthias.r.baer@googlemail.com>
2023-06-30 07:15:39 +0200

    Fix action

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     description=description,
     long_description=open('README.md').read() if exists('README.md') else '',
     long_description_content_type="text/markdown",
-    install_requires=['numpy', 'scipy', 'matplotlib'
+    install_requires=['numpy', 'scipy', 'matplotlib', 'findiff'
                       ],
     python_requires=">=3.6",
     classifiers=['Operating System :: OS Independent',
