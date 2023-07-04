713672842bb94ff3d3fa91dc755dd8e3fc89a0c0
Matthias Baer <matthias.r.baer@googlemail.com>
2023-07-04 17:01:17 +0200

    Test coverage

---
+++ tests/test_plots.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest.mock import patch, Mock
+
+from numgrids import Axis, AxisType, Grid
+from numgrids.plots import Plotter
+
+
+class TestPlotter(unittest.TestCase):
+
+    def test_plot(self):
+        axis = Axis(AxisType.EQUIDISTANT, 30, -1, 1)
+        grid = Grid(axis, axis)
+
+        X, Y = grid.meshed_coords
+        f = X**2 + Y**2
+
+        plotter = Plotter(grid)
+
+        plotter.plot(f, axis.coords, 0)
+        self.assertIsNotNone(plotter.ax)
+
+
