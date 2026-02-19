37cad67d6bd6414ae508c71a1a8d3a319cc1a3a6
Matthias Baer <matthias.r.baer@googlemail.com>
2026-02-19 11:53:08 +0100

    Refactor setup configuration: remove setup.py and migrate to pyproject.toml; update test cases for improved accuracy

--- tests/test_interpol.py
+++ tests/test_interpol.py
@@ -20,20 +20,20 @@ class TestInterpolation(unittest.TestCase):
 
     def test_interpol2d(self):
         grid = Grid(
-            Axis(AxisType.EQUIDISTANT, 50, 0, 1),
-            Axis(AxisType.CHEBYSHEV, 50, 0, 1)
+            Axis(AxisType.EQUIDISTANT, 100, 0, 1),
+            Axis(AxisType.CHEBYSHEV, 100, 0, 1)
         )
         X, Y = grid.meshed_coords
         f = X ** 2 + Y ** 2
         expected = 0.5 ** 2 + 0.5 ** 2
         inter = Interpolator(grid, f)
         actual = inter((0.5, 0.5))
-        self.assertAlmostEqual(actual, expected)
+        self.assertAlmostEqual(actual, expected, places=5)
 
     def test_interpol_polar(self):
         grid = Grid(
-            Axis(AxisType.CHEBYSHEV, 50, 0, 1),
-            Axis(AxisType.EQUIDISTANT_PERIODIC, 50, 0, 2 * np.pi)
+            Axis(AxisType.CHEBYSHEV, 100, 0, 1),
+            Axis(AxisType.EQUIDISTANT_PERIODIC, 100, 0, 2 * np.pi)
         )
         R, Phi = grid.meshed_coords
         f = R ** 2 * np.sin(Phi)
@@ -44,9 +44,9 @@ class TestInterpolation(unittest.TestCase):
 
     def test_interpol_many(self):
         grid = Grid(
-            Axis(AxisType.EQUIDISTANT, 30, 0, 1),
-            Axis(AxisType.CHEBYSHEV, 30, 0, 1),
-            Axis(AxisType.CHEBYSHEV, 30, 0, 1)
+            Axis(AxisType.EQUIDISTANT, 50, 0, 1),
+            Axis(AxisType.CHEBYSHEV, 50, 0, 1),
+            Axis(AxisType.CHEBYSHEV, 50, 0, 1)
         )
         X, Y, Z = grid.meshed_coords
 
@@ -60,7 +60,7 @@ class TestInterpolation(unittest.TestCase):
 
         actual = inter(points)
         expected = [f_(0, 0, 0), f_(0.5, 0.25, 0.5), f_(1, 1, 1)]
-        npt.assert_array_almost_equal(actual, expected)
+        npt.assert_array_almost_equal(actual, expected, decimal=5)
 
     def test_interpol_grid(self):
         fine_grid = Grid(
